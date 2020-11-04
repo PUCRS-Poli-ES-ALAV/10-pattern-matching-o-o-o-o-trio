b114ccca5ad4ca98327b983bd512b7ba7a32a08d
rogoski <pedro.rogoski@edu.pucrs.br>
2020-11-04 20:12:29 -0300

    enunciado 2

--- solution 1/index.js
+++ solution 1/index.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 
 const STRING = 3
-const str1 = fs.readFileSync(`string${STRING}`).toString()
-const str2 = 'messmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmessmesser'
+const str1 = "bom dia grupo choris na area"
+const str2 = 'choris'
 
 let count = 0
+
 function substrIndex(str1, str2) {
     console.log(`str1 length: ${str1.length}`)
     let j = 0
 
-    for (let i = 0; i < str1.length; i ++) {
-        count ++
+    for (let i = 0; i < str1.length; i++) {
+        count++
         if (str1.charAt(i) == str2.charAt(j)) {
             if (j == str2.length - 1) {
                 return i - j
             } else {
-                j ++
+                j++
             }
         } else {
             if (j > 0) {
@@ -30,3 +31,6 @@ function substrIndex(str1, str2) {
 
 const index = substrIndex(str1, str2)
 console.log(`index: ${index}, count: ${count}`)
+
+//Qual a complexidade, no pior caso? str1.length() * str2.length()
+
